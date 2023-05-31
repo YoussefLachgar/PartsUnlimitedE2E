e90b8a9156e0d57f6b1fd81a69bc491e91e33aec
Lachgar Youssef <lachgar.youssef0@gmail.com>
2023-05-31 01:19:58 +0100

    5 first commit.

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        // YL My Scond Change.
+        // My first change test 5
         [Key]
         public int CartItemId { get; set; }
 
